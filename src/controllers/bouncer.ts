5fb1be121b36e6e3a5b0d4934cfb1de6824ba7d7
DANJOCC <danjo2c@gmail.com>
2022-06-14 22:06:50 -0400

    login y register ready >:3

---
+++ src/controllers/bouncer.ts
@@ -0,0 +1,37 @@
+import connection from "../utils/database"
+import Users from "../models/user"
+import {Response, Request} from 'express'
+
+connection()
+
+class Bouncer{
+    public async login(req: Request, res:Response):Promise<void>{
+        let user = await Users.findOne({username:req.body.username})
+        if(user===null)
+            res.status(404).send('No existe usuario')
+        else if(user.password===req.body.password){
+            let token={username:user.username, password:user.password}
+            res.status(200).send(token)
+        }
+        else
+            res.status(404).send('Usuario y contrasena no coinciden')
+    }
+    public async register(req: Request, res:Response, person:Object):Promise<void>{
+        let user = await Users.findOne({email:req.body.email})
+        if(user===null){
+            let newUser=await new Users({
+                username:req.body.username,
+                email:req.body.email,
+                password:req.body.password
+            })
+            await newUser.save()
+            res.status(202).send("usuario registrado")
+        }
+        else if(user.username === req.body.username || user.email===req.body.email){
+            res.status(404).send('Usuario existe, por favor cambie el correo electronico o elija otro nombre de usuario')
+        }
+
+    }
+}
+
+export const entry=new Bouncer()
--- src/routes/rutas.ts
+++ src/routes/rutas.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import { openTrivia } from "../controllers/openTriviaQuestions";
-import {entry} from "../controllers/users"
+import {entry} from "../controllers/bouncer"
 const route: Router = Router();
 
 route.get("/question", openTrivia.getQuestion)
-route.post("/newUser",entry.login)
+route.post("/login",entry.login)
+route.post("/register",entry.register)
 
 export default route;
