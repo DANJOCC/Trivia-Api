c83de56ce5ff93435c5bceef49a6b0f26d9a859d
DANJOCC <danjo2c@gmail.com>
2022-06-18 23:18:07 -0400

    agregue el cors...

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 const app=express();
 import route from './routes/rutas'
 app.set('port', process.env.PORT || 3000);
@@ -7,6 +8,7 @@ app.set('host', process.env.HOST || 'localhost')
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(route)
+app.use(cors())
 
 app.listen(app.get('port'), ()=>{
     console.log(`Server on ${app.get('host')}/${app.get('port')}`)
