2213f852bfd3d4667e0a22050549308795ca2f32
DANJOCC <danjo2c@gmail.com>
2022-06-18 23:42:16 -0400

    lo que dios quiera...

--- build/src/index.js
+++ build/src/index.js
@@ -9,10 +9,10 @@ const app = (0, express_1.default)();
 const rutas_1 = __importDefault(require("./routes/rutas"));
 app.set('port', process.env.PORT || 3000);
 app.set('host', process.env.HOST || 'localhost');
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(rutas_1.default);
-app.use((0, cors_1.default)());
 app.listen(app.get('port'), () => {
     console.log(`Server on ${app.get('host')}/${app.get('port')}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ const app=express();
 import route from './routes/rutas'
 app.set('port', process.env.PORT || 3000);
 app.set('host', process.env.HOST || 'localhost')
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(route)
-app.use(cors())
+
 
 app.listen(app.get('port'), ()=>{
     console.log(`Server on ${app.get('host')}/${app.get('port')}`)
